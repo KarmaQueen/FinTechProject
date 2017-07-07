33aff5c469ccad129c1fad46a634ff6cffff9e86
David Lee <leetdavid@outlook.com>
2017-07-08 07:38:17 +0800

    Added basic AccountType and LoanInfoBean

---
+++ app/src/main/java/paraverity/com/fintechproject/AccountType.java
@@ -0,0 +1,23 @@
+package paraverity.com.fintechproject;
+
+/**
+ * Created by David on 08-Jul-17.
+ */
+
+public class AccountType {
+
+	public static final String
+			HSBCPremier = "HSBC Premier",
+			HSBCAdvanced = "HSBC Advanced",
+			HangSeng = "Hang Seng Bank",
+			Citic = "Citic Bank Intl",
+			WingLung = "Wing Lung",
+			DBS = "DBS",
+			Construction = "China Construction",
+			ICBC = "ICBC",
+			Fubon = "Fubon",
+			BOCHK = "Bank of China HK",
+			BEA = "Bank of East Asia",
+			CitiBank = "CitiBank";
+
+}
