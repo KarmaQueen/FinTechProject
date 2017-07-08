0940113bcaf77cebdaf6006f350f9c3c7b0c8358
David Lee <leetdavid@outlook.com>
2017-07-08 12:12:19 +0800

    Updated LoginActivity

--- app/src/main/java/paraverity/com/fintechproject/LoginActivity.java
+++ app/src/main/java/paraverity/com/fintechproject/LoginActivity.java
@@ -2,6 +2,7 @@ package paraverity.com.fintechproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class LoginActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+	}
+
+	protected void sendSMS(View view){
+
+	}
+
+	protected void doLogin(View view){
+
 	}
 }
