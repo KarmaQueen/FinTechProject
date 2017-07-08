edfd6487d0cb1d57ad19677953db2167ddb87532
keyikeyi <tankeyi1997@hotmail.com>
2017-07-08 19:25:14 +0800

    Added "sort" button (partially complete)

--- app/src/main/java/paraverity/com/fintechproject/bankRecommendation.java
+++ app/src/main/java/paraverity/com/fintechproject/bankRecommendation.java
@@ -1,13 +1,22 @@
 package paraverity.com.fintechproject;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Button;
 
 public class bankRecommendation extends AppCompatActivity {
+
+    private RadioGroup chooseFilter;
+    private Button buttonClicked;
+    private RadioButton selectedBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +26,27 @@ public class bankRecommendation extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        addListenerOnChoice();
     }
 
+    private void addListenerOnChoice() {
+        chooseFilter = (RadioGroup) findViewById(R.id.filterLoan);
+        buttonClicked = (Button) findViewById(R.id.sortBtn);
+
+        buttonClicked.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                int selectedID = chooseFilter.getCheckedRadioButtonId();
+                selectedBtn = (RadioButton) findViewById(selectedID);
+                // rank data differently
+            }
+        });
+    }
 }
