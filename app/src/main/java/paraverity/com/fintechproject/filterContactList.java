8b249d24c18f675886f89c384aca35b6686e43c6
Ignavier <ignavierng@gmail.com>
2017-07-08 12:50:31 +0800

    v1

---
+++ app/src/main/java/paraverity/com/fintechproject/filterContactList.java
@@ -0,0 +1,31 @@
+package paraverity.com.fintechproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class filterContactList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_contact_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btn = (Button)findViewById(R.id.friend_list_confirmation);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(filterContactList.this, MainActivity.class));
+            }
+        });
+    }
+}
